62e7b23725f7007a83e9d88e6eec3be67612e16e
nurlinaberra <46892713+nurlinaberra@users.noreply.github.com>
2024-09-24 21:13:50 +0200

    Update rock_paper_scissors.py

--- Week2/rock_paper_scissors.py
+++ Week2/rock_paper_scissors.py
@@ -13,17 +13,22 @@ class RockPaperScissors:
         # Tar spelarens val och säkerställer att det är giltigt
         choice = input("Välj sten, sax eller påse: ").lower()
         while choice not in self.choices:
+            # Om spelaren inte har valt något av de tre valen så frågas spelaren igen
             choice = input("Ogiltigt val. Välj sten, sax eller påse: ").lower()
         return choice
 
     def determine_winner(self, player, computer):
         # Bestämmer vinnaren baserat på reglerna
+       
         if player == computer:
+             # Om spelaren och datorn har samma val så är det oavgjort
             return "Oavgjort"
+        # Om spelaren har valt sten och datorn har valt sax så vinner spelaren  
         elif (player == 'sten' and computer == 'sax') or \
              (player == 'sax' and computer == 'påse') or \
              (player == 'påse' and computer == 'sten'):
             return "Spelaren vinner"
+        
         else:
             return "Datorn vinner"
 
