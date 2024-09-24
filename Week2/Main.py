ee7b41bb2b2bcff925f0845cddffdf261959341c
nurlinaberra <46892713+nurlinaberra@users.noreply.github.com>
2024-09-24 21:13:53 +0200

    Update Main.py

--- Week2/Main.py
+++ Week2/Main.py
@@ -2,5 +2,6 @@ print("Test main hello world new code:")
 from rock_paper_scissors import RockPaperScissors
 
 if __name__ == "__main__":
+     # Skapar en instans av RockPaperScissors och kör spelet
     game = RockPaperScissors()
     game.play_game()
